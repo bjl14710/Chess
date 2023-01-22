5e931c93ed24ed9b7fbc96cd61530dcf94e64a49
bjl14710 <bjl14710@gmail.com>
2023-01-21 17:28:04 -0700

    added moveRook

--- ChessGamed.py
+++ ChessGamed.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 class Chess:
     def __init__(self):
         #8x8
@@ -34,30 +36,29 @@ class Chess:
                     return [r,c]
         return [-1,-1]
 
-    def movePawn(self, select, move, color) -> bool:
+    def movePawn(self, select, direction, color) -> bool:
         # determine specific pawn.
         ROWS, COLS = len(self.board), len(self.board[0])
-        red_directions = {"forward": (1,0), "back": (-1,0), "left":(0,-1),"right":(1,0)}
-        black_directions = {"forward": (-1,0), "back": (1,0), "left":(0,1),"right":(-1,0)}
+        red_directions = {"forward": (1,0), "back": (-1,0), "left":(0,1),"right":(0,-1)}
+        black_directions = {"forward": (-1,0), "back": (1,0), "left":(0,-1),"right":(0,1)}
         r,c = self.findPiece("pawn",color,select)
-        print(r,c)
         if r == -1 and c == -1:
             print("piece not found")
             return 
         if color == "black":
             # use black_directions
-            dr,dc = black_directions[move]
+            dr,dc = black_directions[direction]
             Row = r + dr
             Col = c + dc
         elif color == "red":
-            dr, dc = red_directions[move]
+            dr, dc = red_directions[direction]
             Row = r + dr
             Col = c + dc
             # use red directions
         else:
             print("not found")
             return
-        # make the move
+        # make the move This should be it's own function since it will be used a lot.
         if (Row not in range(ROWS) or 
             Col not in range(COLS) or 
             self.board[Row][Col][1] == color):
@@ -66,25 +67,57 @@ class Chess:
             print("move not legal")
             return
         # else, let's modify the board to put the pawn there
-        temp = self.board[r][c]
-        # empty board piece
-        self.board[r][c] = (0,"",0)
-        self.board[Row][Col] = temp
-
+        # this swap should be too.
+        self.swap(r,c,Row,Col)
+        
     def moveBishop(self):
         return
     
-    def moveKnight(self):
+    def moveKnight(self,direction,move,color):
         return
     
-    def moveKing(self):
+    def moveKing(self,direction):
         return
 
-    def moveQueen(self):
+    def moveQueen(self,direction,move):
         return
     
-    def moveRook(self):
-        return
+    def moveRook(self,direction,move,color,select):
+        notColor = color
+        red_directions = {"forward": (1,0), "back": (-1,0), "left":(0,1),"right":(0,-1)}
+        black_directions = {"forward": (-1,0), "back": (1,0), "left":(0,-1),"right":(0,1)}
+        
+        if color == "black":
+            notColor = "red"
+            dr, dc = black_directions[direction]
+        elif color == "red":
+            notColor = "black"
+            dr, dc = red_directions[direction]
+        row,col = self.findPiece("rook",color,select)
+        
+        def DFSMove(r,c,movesLeft):
+            nonlocal dr, dc, notColor, row, col
+            if movesLeft == 0:
+                self.swap(row,col,r,c)
+                return True
+            if (r not in range(self.rows) or c not in range(self.cols)
+                or (self.board[r][c][1] == color and (r != row or c != col))):
+                print("move not legal")
+                # can't kill our own pieces or move outside of the board
+                return False
+            if (self.board[r][c][1] == notColor):
+                # we killed a piece
+                self.swap(row,col,r,c)
+                return False
+            DFSMove(r+dr,c+dc,movesLeft-1)
+        # we reached the end
+        DFSMove(row,col,move)
+
+    def swap(self,oR,oC,nR,nC):
+        temp = self.board[oR][oC]
+        self.board[oR][oC] = self.board[nR][nC]
+        self.board[nR][nC] = temp
+
     def printBoard(self):
         for r in range(self.rows):
             print(self.board[r])
@@ -93,9 +126,18 @@ if __name__ == "__main__":
     Game = Chess()
     # specific pawn, move desired, color
     Game.printBoard()
-    Game.movePawn(0,"forward","black")
-    
+    Game.movePawn(0,"forward","red")
+    Game.movePawn(0,"left","red")
+    Game.movePawn(1,"forward","black")
+    Game.movePawn(1,"forward","black")
+    Game.moveRook("forward",4,"red",0)
+    print()
+    print()
+    print()
+    # movePawn(self, select, move, color)
+    # moveRook(self,direction,move,color,select)
     Game.printBoard()
+    
     
     
 
